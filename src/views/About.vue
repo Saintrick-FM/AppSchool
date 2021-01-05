27e00b9ad2d190a05268eb9e98c328ee18b7d554
Saintrick-FM <francymalonga8@gmail.com>
2021-01-05 14:25:17 +0000

    Projet initial avec personnalisation des components

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
+    <Squelette/>
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Squelette from '@/components/Squelette.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Squelette
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Squelette/>
+    <h1>Bienvenu sur la page d'accueil (home) du logiciel</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Squelette from '@/components/Squelette.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Squelette
   }
 }
 </script>
